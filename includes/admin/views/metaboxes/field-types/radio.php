f743da55c9effc589b15c6c9497cb3daf11b19d4
Kathy Darling <helgatheviking@gmail.com>
2020-05-26 19:42:38 -0600

    Use a fieldset to provide better instructions for radio fields
    (cherry picked from commit ce859d3ebdf27fb31c96b85305a371e873a6fe0e)
    (cherry picked from commit b1893fa72712a9007621575187cf97781322a22f)

--- includes/admin/views/metaboxes/field-types/radio.php
+++ includes/admin/views/metaboxes/field-types/radio.php
@@ -18,9 +18,10 @@ $field_attrs = array_key_exists( 'field_attrs', $view_args ) ? $view_args['field
 
 ?>
 <div id="<?php echo esc_attr( $view_args['wrapper_id'] ); ?>" class="<?php echo esc_attr( $view_args['wrapper_class'] ); ?>" <?php echo charitable_get_arbitrary_attributes( $view_args ); ?>>
-	<?php if ( isset( $view_args['label'] ) ) : ?>
-		<label for="<?php echo esc_attr( $view_args['id'] ); ?>" id="charitable_field_<?php echo esc_attr( $view_args['key'] ); ?>_label">
-			<?php
+	<fieldset class="charitable-radio-fieldset">
+
+	<?php if ( isset( $view_args['label'] ) || isset( $view_args['description'] ) ) : ?>
+		<legend><?php
 			echo esc_html( $view_args['label'] );
 			if ( $is_required ) :
 				?>
@@ -28,6 +29,10 @@ $field_attrs = array_key_exists( 'field_attrs', $view_args ) ? $view_args['field
 				<?php
 			endif;
 			?>
+		</legend>
+		<?php if ( isset( $view_args['description'] ) ) : ?>
+			<span class="charitable-helper"><?php echo esc_html( $view_args['description'] ); ?></span>
+		<?php endif ?>
 		</label>
 	<?php endif ?>
 	<ul class="charitable-radio-list">
@@ -45,7 +50,5 @@ $field_attrs = array_key_exists( 'field_attrs', $view_args ) ? $view_args['field
 		</li>
 	<?php endforeach ?>
 	</ul>
-	<?php if ( isset( $view_args['description'] ) ) : ?>
-		<span class="charitable-helper"><?php echo esc_html( $view_args['description'] ); ?></span>
-	<?php endif ?>
+	</fieldset>
 </div><!-- #<?php echo $view_args['wrapper_id']; ?> -->
