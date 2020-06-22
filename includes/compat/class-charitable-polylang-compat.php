512adc5963d3ec96a68b4bd3ffe69943e36b0975
Eric Daams <eric@164a.com>
2020-06-22 02:01:33 +0000

    Pick up the current language version of various Charitable endpoints and pages when translations are made with Polylang. Closes #790.

--- includes/compat/class-charitable-polylang-compat.php
+++ includes/compat/class-charitable-polylang-compat.php
@@ -25,6 +25,15 @@ if ( ! class_exists( 'Charitable_Polylang_Compat' ) ) :
 	class Charitable_Polylang_Compat {
 
 		/**
+		 * Endpoints that can have Polylang variants.
+		 *
+		 * @since 1.6.42
+		 *
+		 * @var   array
+		 */
+		private $endpoints;
+
+		/**
 		 * Create class object.
 		 *
 		 * @since 1.6.42
@@ -32,9 +41,25 @@ if ( ! class_exists( 'Charitable_Polylang_Compat' ) ) :
 		public function __construct() {
 			add_action( 'wp', array( $this, 'load_late_translations' ) );
 
-			/* Get Polylang translation of endpoints. */
+			/* Profile Page */
 			add_filter( 'charitable_permalink_profile_page', array( $this, 'get_profile_page_url' ), 10, 2 );
 			add_filter( 'charitable_is_page_profile_page', array( $this, 'is_profile_page' ), 10, 2 );
+
+			/* Login Page */
+			add_filter( 'charitable_permalink_login_page', array( $this, 'get_login_page_url' ), 10, 2 );
+			add_filter( 'charitable_is_page_login_page', array( $this, 'is_login_page' ), 10, 2 );
+
+			/* Registration Page */
+			add_filter( 'charitable_permalink_registration_page', array( $this, 'get_registration_page_url' ), 10, 2 );
+			add_filter( 'charitable_is_page_registration_page', array( $this, 'is_registration_page' ), 10, 2 );
+
+			/* Donation Receipt Page */
+			add_filter( 'charitable_permalink_donation_receipt_page', array( $this, 'get_donation_receipt_page_url' ), 10, 2 );
+			add_filter( 'charitable_is_page_donation_receipt_page', array( $this, 'is_donation_receipt_page' ), 10, 2 );
+
+			/* Terms & Conditions and Privacy Policy */
+			add_filter( 'charitable_option_terms_conditions_page', array( $this, 'get_polylang_page_id' ) );
+			add_filter( 'charitable_option_privacy_policy_page', array( $this, 'get_polylang_page_id' ) );
 		}
 
 		/**
@@ -115,15 +140,17 @@ if ( ! class_exists( 'Charitable_Polylang_Compat' ) ) :
 		}
 
 		/**
-		 * Get the profile page URL.
+		 * Get the current language version of the specified endpoint,
+		 * or return the default.
 		 *
 		 * @since  1.6.42
 		 *
-		 * @param  string $default The endpoint's URL.
-		 * @param  array  $args    Mixed set of arguments.
+		 * @param  string $endpoint_id The endpoint ID.
+		 * @param  string $default     The endpoint's URL.
+		 * @param  array  $args        Mixed set of arguments.
 		 * @return string
 		 */
-		public function get_profile_page_url( $default, $args ) {
+		public function get_polylang_page_url( $page_option, $default, $args ) {
 			if ( empty( $default ) ) {
 				return $default;
 			}
@@ -133,21 +160,26 @@ if ( ! class_exists( 'Charitable_Polylang_Compat' ) ) :
 				return $default;
 			}
 
-			$pll_page = pll_get_post( charitable_get_option( 'profile_page', false ) );
+			$page_id = charitable_get_option( $page_option, false );
 
-			return get_permalink( $pll_page );
+			if ( in_array( $page_id, array( 'wp', 'auto' ) ) ) {
+				return $default;
+			}
+
+			return get_permalink( pll_get_post( $page_id ) );
 		}
 
 		/**
-		 * Check whether we are currently on the profile page.
+		 * Checks whether this is the specified page.
 		 *
 		 * @since  1.6.42
 		 *
-		 * @param  boolean $is_page Whether we are currently on the profile page.
-		 * @param  array   $args    Mixed arguments.
-		 * @return boolean
+		 * @param  string  $page_option The option key used to record the page ID.
+		 * @param  boolean $is_page     Whether we are currently on the page.
+		 * @param  array   $args        Mixed arguments.
+		 * @return string
 		 */
-		public function is_profile_page( $is_page, $args ) {
+		public function is_polylang_page( $page_option, $is_page, $args ) {
 			/* We've already determined it's the current page. */
 			if ( $is_page ) {
 				return $is_page;
@@ -161,12 +193,146 @@ if ( ! class_exists( 'Charitable_Polylang_Compat' ) ) :
 			global $post, $wp_query;
 
 			if ( is_null( $post ) || ! $wp_query->is_main_query() ) {
-				return false;
+				return $is_page;
 			}
 
-			$page = pll_get_post( charitable_get_option( 'profile_page', false ) );
+			$page_id = charitable_get_option( $page_option, false );
 
-			return $page && $page === $post->ID;
+			if ( in_array( $page_id, array( 'wp', 'auto' ) ) ) {
+				return $is_page;
+			}
+
+			$pll_page = pll_get_post( $page_id );
+
+			return $pll_page && $pll_page === $post->ID;
+		}
+
+		/**
+		 * Get the profile page URL.
+		 *
+		 * @since  1.6.42
+		 *
+		 * @param  string $default The endpoint's URL.
+		 * @param  array  $args    Mixed set of arguments.
+		 * @return string
+		 */
+		public function get_profile_page_url( $default, $args ) {
+			return $this->get_polylang_page_url( 'profile_page', $default, $args );
+		}
+
+		/**
+		 * Check whether we are currently on the profile page.
+		 *
+		 * @since  1.6.42
+		 *
+		 * @param  boolean $is_page Whether we are currently on the profile page.
+		 * @param  array   $args    Mixed arguments.
+		 * @return boolean
+		 */
+		public function is_profile_page( $is_page, $args ) {
+			return $this->is_polylang_page( 'profile_page', $is_page, $args );
+		}
+
+		/**
+		 * Get the login page URL.
+		 *
+		 * @since  1.6.42
+		 *
+		 * @param  string $default The endpoint's URL.
+		 * @param  array  $args    Mixed set of arguments.
+		 * @return string
+		 */
+		public function get_login_page_url( $default, $args ) {
+			return $this->get_polylang_page_url( 'login_page', $default, $args );
+		}
+
+		/**
+		 * Check whether we are currently on the login page.
+		 *
+		 * @since  1.6.42
+		 *
+		 * @param  boolean $is_page Whether we are currently on the login page.
+		 * @param  array   $args    Mixed arguments.
+		 * @return boolean
+		 */
+		public function is_login_page( $is_page, $args ) {
+			return $this->is_polylang_page( 'login_page', $is_page, $args );
+		}
+
+		/**
+		 * Get the registration page URL.
+		 *
+		 * @since  1.6.42
+		 *
+		 * @param  string $default The endpoint's URL.
+		 * @param  array  $args    Mixed set of arguments.
+		 * @return string
+		 */
+		public function get_registration_page_url( $default, $args ) {
+			return $this->get_polylang_page_url( 'registration_page', $default, $args );
+		}
+
+		/**
+		 * Check whether we are currently on the registration page.
+		 *
+		 * @since  1.6.42
+		 *
+		 * @param  boolean $is_page Whether we are currently on the registration page.
+		 * @param  array   $args    Mixed arguments.
+		 * @return boolean
+		 */
+		public function is_registration_page( $is_page, $args ) {
+			return $this->is_polylang_page( 'registration_page', $is_page, $args );
+		}
+
+		/**
+		 * Get the donation receipt page URL.
+		 *
+		 * @since  1.6.42
+		 *
+		 * @param  string $default The endpoint's URL.
+		 * @param  array  $args    Mixed set of arguments.
+		 * @return string
+		 */
+		public function get_donation_receipt_page_url( $default, $args ) {
+			$pll_url = $this->get_polylang_page_url( 'donation_receipt_page', $default, $args );
+
+			if ( $pll_url !== $default ) {
+				$donation_id = isset( $args['donation_id'] ) ? $args['donation_id'] : get_the_ID();
+				$pll_url     = esc_url_raw( add_query_arg( array( 'donation_id' => $donation_id ), $pll_url ) );
+			}
+
+			return $pll_url;
+		}
+
+		/**
+		 * Check whether we are currently on the donation receipt page.
+		 *
+		 * @since  1.6.42
+		 *
+		 * @param  boolean $is_page Whether we are currently on the donation_receipt page.
+		 * @param  array   $args    Mixed arguments.
+		 * @return boolean
+		 */
+		public function is_donation_receipt_page( $is_page, $args ) {
+			return $this->is_polylang_page( 'donation_receipt_page', $is_page, $args );
+		}
+
+		/**
+		 * Get the page ID of a specific page, returning the Polylang
+		 * translation if applicable.
+		 *
+		 * @since  1.6.42
+		 *
+		 * @param  int|string $page_id The set page id.
+		 * @return int|string
+		 */
+		public function get_polylang_page_id( $page_id ) {
+			if ( empty( $page_id ) || 0 == $page_id ) {
+				return $page_id;
+			}
+
+			return pll_get_post( $page_id );
 		}
 	}
 
